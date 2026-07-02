73e639384c9320b2b8f78952cdc87762691a4425
Jose Ignacio Carretero <joseignacio.carretero@gmail.com>
2026-07-02 12:45:47 +0200

    Start working with forbidden pushes to main

--- tests/test_unencode_uris.py
+++ tests/test_unencode_uris.py
@@ -16,17 +16,29 @@ class MyTestCase(unittest.TestCase):
         sa = SubjectAnalysis(self.o)
 
         for f in sa:
-            assert(f['id'] == 'urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11:19:58')
-            assert(f['https://saref.etsi.org/core/hasResult']['object'] == 'urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11:19:58:PropertyValue')
+            assert (
+                f["id"]
+                == "urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11:19:58"
+            )
+            assert (
+                f["https://saref.etsi.org/core/hasResult"]["object"]
+                == "urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11:19:58:PropertyValue"
+            )
             print(f)
 
-    def test_with_encodetransform_unquote_true(self):
+    def test_with_encodetransform_unquote_false(self):
         ConfigTranslator.instance = None
         ConfigTranslator("configs/test_config_no_transform.cfg")
 
         sa = SubjectAnalysis(self.o)
 
         for f in sa:
-            assert(f['id'] == 'urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11%3a19%3a58')
-            assert(f['https://saref.etsi.org/core/hasResult']['object'] == 'urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11%3a19%3a58:PropertyValue')
+            assert (
+                f["id"]
+                == "urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11%3a19%3a58"
+            )
+            assert (
+                f["https://saref.etsi.org/core/hasResult"]["object"]
+                == "urn:Pilot5:Device:10502070602:EnergyObservation:2024-10-31T11%3a19%3a58:PropertyValue"
+            )
             print(f)
